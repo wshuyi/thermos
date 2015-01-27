13dcd5078e7933c574c71d7657ad08f0a3887e25
Wang Shuyi <nkwshuyi@gmail.com>
2015-01-27 15:58:19 -0500

    error handler added in thermos.py

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -18,5 +18,14 @@ def index():
 def add():
     return render_template('add.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
