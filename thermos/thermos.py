fde68d44f1ab1549267650f1661e38a4dfd8f3ef
Wang Shuyi <nkwshuyi@gmail.com>
2015-01-27 21:14:34 -0500

    not run app on 0.0.0.0 now for safety reason

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -54,4 +54,5 @@ def server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+#    app.run(host="0.0.0.0", debug=True)
