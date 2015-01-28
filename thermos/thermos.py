17cd5e048f6bef9019d7f063ae00182754152b87
Wang Shuyi <nkwshuyi@gmail.com>
2015-01-27 20:48:28 -0500

    added flask_wtf to handle forms

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect, flash
 
+from forms import BookmarkForm
+
 #from logging import DEBUG
 
 app = Flask(__name__)
@@ -8,9 +10,10 @@ app = Flask(__name__)
 bookmarks = []
 app.config['SECRET_KEY'] = 'l\xa9\x8e\xfc\xf0\x0b\x92\x07\x7f\x1e\xa5\xbd\xa3A\x00\x9c\x86\xde\xff+G\xee\x8f~'
 
-def store_bookmark(url):
+def store_bookmark(url, description):
     bookmarks.append(dict(
         url = url,
+        description = description,
         user = "reindert",
         date = datetime.utcnow()
         ))
@@ -32,12 +35,14 @@ def index():
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
-    if request.method == "POST":
-        url = request.form['url']
-        store_bookmark(url)
-        flash("Stored bookmark '{}'".format(url))
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template('add.html')
+    return render_template('add.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
